7e3bd0f6cc277a7dbf7e733ddff6754230a9903d
bennebw <bennebw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-23 20:26:55 +0000

    Fixed an Install issue also related to the folder name.
    git-svn-id: https://plugins.svn.wordpress.org/photosmash-galleries/trunk@88758 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bwb-photosmash.php
+++ bwb-photosmash.php
@@ -3,7 +3,7 @@
 Plugin Name: PhotoSmash
 Plugin URI: http://www.whypad.com/posts/photosmash-galleries-wordpress-plugin-released/507/
 Description: PhotoSmash - user contributable photo galleries for WordPress pages and posts.  Auto-add galleries to posts or specify with simple tags.  Utilizes class.upload.php by Colin Verot at http://www.verot.net/php_class_upload.htm, licensed GPL.  PhotoSmash is licensed under the GPL.
-Version: 0.1.2
+Version: 0.1.3
 Author: Byron Bennett
 Author URI: http://www.whypad.com/
 */
@@ -495,7 +495,7 @@ add_action('admin_menu', 'bwbpsAdminPage');
 
 
 //Call the INIT function whenever the Plugin is activated
-add_action('activate_bwb-photosmash/bwb-photosmash.php',
+add_action('activate_photosmash-galleries/bwb-photosmash.php',
 array(&$bwbPS, 'init'));
 
 
