92e8278c87863b14e28c7fd1cc3bef115744644e
bennebw <bennebw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-16 05:23:08 +0000

    Added Plugin Info page to aid in debugging and trouble shooting for Blog owners.
    git-svn-id: https://plugins.svn.wordpress.org/photosmash-galleries/trunk@110968 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bwb-photosmash.php
+++ bwb-photosmash.php
@@ -3,7 +3,7 @@
 Plugin Name: PhotoSmash
 Plugin URI: http://www.whypad.com/posts/photosmash-galleries-wordpress-plugin-released/507/
 Description: PhotoSmash - user contributable photo galleries for WordPress pages and posts.  Auto-add galleries to posts or specify with simple tags.  Utilizes class.upload.php by Colin Verot at http://www.verot.net/php_class_upload.htm, licensed GPL.  PhotoSmash is licensed under the GPL.
-Version: 0.2.54
+Version: 0.2.55
 Author: Byron Bennett
 Author URI: http://www.whypad.com/
 */
@@ -58,7 +58,7 @@ class BWB_PhotoSmash{
 	
 	//Called when plugin is activated
 	function init(){
-		require_once('bwbps-init.php');
+		require_once('admin/bwbps-init.php');
 		$bwbpsinit = new BWBPS_Init();					
 	}
 	
@@ -122,6 +122,9 @@ class BWB_PhotoSmash{
 			
 			add_submenu_page(basename(__FILE__), __('Database Viewer'), __('Photo Manager'), 9,  
 			'managePhotoSmashImages', array(&$bwbPS, 'loadPhotoManager'));
+			
+			add_submenu_page(basename(__FILE__), __('Plugin Info'), __('Plugin Info'), 9,  
+			'psInfo', array(&$bwbPS, 'loadPsInfo'));
 		}
 		
 	}
@@ -151,6 +154,12 @@ class BWB_PhotoSmash{
 		}
 		$this->psAdmin->printManageImages();
 		
+		return true;
+	}
+	
+	function loadPSInfo(){
+		require_once("admin/bwbps-info.php");
+		$ts = new BWBPS_Info();			
 		return true;
 	}
 	
