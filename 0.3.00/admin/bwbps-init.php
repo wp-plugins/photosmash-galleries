577eff37f5f71e85c805a1be1281563dea3326b1
bennebw <bennebw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-13 05:03:35 +0000

    Official release of PhotoSmash 0.3
    git-svn-id: https://plugins.svn.wordpress.org/photosmash-galleries/tags@134593 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 0.3.00/admin/bwbps-init.php
@@ -0,0 +1,239 @@
+<?php 
+
+//Code for initializing BWB-PhotoSmash plugin when it is activated in Wordpress Plugins admin page
+class BWBPS_Init{
+	
+	var $adminOptionsName = "BWBPhotosmashAdminOptions";
+	
+	//Constructor
+	function BWBPS_Init(){
+		//Create the PhotoSmash Tables if not exists
+		
+		global $wpdb;
+		
+		$table_name = $wpdb->prefix . "bwbps_images";
+				
+			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+			
+			if ( $wpdb->has_cap( 'collation' ) ) {
+				if ( ! empty($wpdb->charset) )
+					$charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+				if ( ! empty($wpdb->collate) )
+					$charset_collate .= " COLLATE $wpdb->collate";
+			}
+			
+			//Create the Images table
+			$sql = "CREATE TABLE " . $table_name . " (
+				image_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+				gallery_id BIGINT(20) NOT NULL,
+				user_id BIGINT(20) NOT NULL,
+				post_id BIGINT(20),
+				comment_id BIGINT(20) NOT NULL,
+				image_name VARCHAR(250) NOT NULL,
+				image_caption TEXT,
+				file_type TINYINT(1),
+				file_name TEXT NOT NULL,
+				file_url TEXT,
+				url VARCHAR(250) NOT NULL,
+				custom_fields TEXT,
+				updated_by BIGINT(20) NOT NULL,
+				created_date DATETIME NOT NULL,
+				updated_date TIMESTAMP NOT NULL,
+				status TINYINT(1) NOT NULL,
+				alerted TINYINT(1) NOT NULL,
+				seq BIGINT(11) NOT NULL,
+				avg_rating FLOAT(8,4) NOT NULL,
+				rating_cnt BIGINT(11) NOT NULL,
+				PRIMARY KEY   (image_id),
+				INDEX (gallery_id)
+				)  $charset_collate;";
+			dbDelta($sql);
+			
+			$sql = "ALTER TABLE " . $wpdb->prefix."bwbps_imageratings ".
+				"DROP INDEX image_id";
+			$wpdb->query($sql);
+			
+						
+			//Create the Gallery Table
+			$table_name = $wpdb->prefix . "bwbps_galleries";
+			$sql = "CREATE TABLE " . $table_name . " (
+				gallery_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+				post_id BIGINT(20),
+				gallery_name VARCHAR(250),
+				gallery_type TINYINT(1),
+				caption TEXT,
+				add_text VARCHAR(250),
+				upload_form_caption VARCHAR(250),
+				contrib_role TINYINT(1) NOT NULL,
+				img_rel VARCHAR(255),
+				img_class VARCHAR(255),
+				img_perrow TINYINT(1),
+				img_perpage TINYINT(1),
+				thumb_aspect TINYINT(1),
+				thumb_width INT(4),
+				thumb_height INT(4),
+				image_aspect TINYINT(1),
+				image_width INT(4),
+				image_height INT(4),
+				show_caption TINYINT(1),
+				nofollow_caption TINYINT(1),
+				caption_template VARCHAR(255),
+				show_imgcaption TINYINT(1),
+				img_status TINYINT(1),
+				created_date DATETIME NOT NULL,
+				updated_date TIMESTAMP NOT NULL,
+				layout_id INT(4),
+				use_customform TINYINT(1),
+				custom_formid INT(4),
+				use_customfields TINYINT(1),
+				cover_imageid BIGINT(20),
+				status TINYINT(1),
+				sort_field TINYINT(1),
+				sort_order TINYINT(1),
+				PRIMARY KEY  (gallery_id))
+				$charset_collate
+				;";
+			dbDelta($sql);
+			
+			//Create the Image Ratings table (future use)
+			$table_name = $wpdb->prefix . "bwbps_imageratings";
+			$sql = "CREATE TABLE " . $table_name . " (
+				rating_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+				image_id BIGINT(20) NOT NULL,
+				user_id BIGINT(20) NOT NULL,
+				rating TINYINT(1) NOT NULL,
+				comment VARCHAR(250) NOT NULL,
+				updated_date TIMESTAMP NOT NULL,
+				status TINYINT(1) NOT NULL,
+				PRIMARY KEY  (rating_id),
+				INDEX (image_id)
+				)  $charset_collate;";
+			dbDelta($sql);
+			
+			/* Create the PhotoSmash HTML layouts table
+			* HTML layouts are templates that lets you
+			* create a predefined HTML layout with shortcodes to display
+			* galleries
+			*/
+			$sql = "CREATE TABLE " . $wpdb->prefix."bwbps_layouts (
+				layout_id INT(11) NOT NULL AUTO_INCREMENT,
+				layout_name VARCHAR(30) ,
+				layout TEXT ,
+				alt_layout TEXT ,
+				wrapper TEXT ,
+				cells_perrow TINYINT NOT NULL default '0',
+				css TEXT ,
+				lists VARCHAR(255) ,
+				fields_used TEXT,
+				PRIMARY KEY  (layout_id)
+				)  $charset_collate;";
+			dbDelta($sql);
+			
+			
+			/* Create the CUSTOM FORMS table
+			* Custom forms let you create any form layout you can imagine...I think
+			* Code safe!
+			*/
+			$sql = "CREATE TABLE " . $wpdb->prefix."bwbps_forms (
+				form_id INT(11) NOT NULL AUTO_INCREMENT,
+				form_name VARCHAR(30) ,
+				form TEXT ,
+				css TEXT ,
+				fields_used TEXT,
+				category TINYINT(1),
+				PRIMARY KEY  (form_id)
+				)  $charset_collate;";
+			dbDelta($sql);
+			
+			
+						
+			//Create the Fields table
+			$sql = "CREATE TABLE " . $wpdb->prefix."bwbps_fields (
+				field_id INT(11) NOT NULL AUTO_INCREMENT,
+				form_id INT(4) NOT NULL default '0',
+				field_name VARCHAR(50) ,
+				label VARCHAR(255) ,
+				type INT(4) ,
+				numeric_field TINYINT(1) NOT NULL default '0',
+				multi_val TINYINT(1) NOT NULL,
+				default_val varchar(255),
+				html_filter TINYINT(1),
+				date_format TINYINT(1),
+				seq INT(4) ,
+				status TINYINT(1) NOT NULL ,
+				PRIMARY KEY  (field_id)
+				)  $charset_collate;";
+			
+			dbDelta($sql);
+			
+			$sql = "ALTER TABLE " . $wpdb->prefix."bwbps_lookup ".
+				"DROP INDEX field_id";
+			$wpdb->query($sql);
+			
+			//Create the Supple Lookup Table
+			$sql = "CREATE TABLE " . $wpdb->prefix."bwbps_lookup (
+				id INT(11) NOT NULL AUTO_INCREMENT,
+				field_id INT(4) ,
+				value VARCHAR(255) ,
+				label VARCHAR(255) ,
+				seq INT(4) ,
+				PRIMARY KEY   (id),
+				INDEX (field_id)
+				)  $charset_collate;";
+			dbDelta($sql);
+			
+			//SQL for table creation & updating
+			$sql = "CREATE TABLE " . $wpdb->prefix."bwbps_customdata (
+				id INT(11) NOT NULL AUTO_INCREMENT,
+				image_id INT(11) NOT NULL,
+				updated_date TIMESTAMP NOT NULL, 
+				bwbps_status TINYINT(1) NOT NULL default '0',
+				PRIMARY KEY  (id)
+				)  $charset_collate;";
+			dbDelta($sql);
+						
+		//Neeed to Set PS Default Options
+		$this->getPSDefaultOptions();
+	}
+	
+	//Returns an array of default options
+	function getPSDefaultOptions()
+	{
+		$psOptions = get_option($this->adminOptionsName);
+		if(!empty($psOptions))
+		{
+			//Options were found..add them to our return variable array
+			foreach ( $psOptions as $key => $option ){
+				$psAdminOptions[$key] = $option;
+			}
+		}else{
+			$psAdminOptions = array(
+				'auto_add' => 0,
+				'img_perrow' => 0,
+				'img_perpage' => 0,
+				'thumb_width' => 110,
+				'thumb_height' => 110,
+				'img_rel' => 'lightbox',
+				'add_text' => 'Add Photo',
+				'gallery_caption' => 'PhotoSmash Gallery',
+				'upload_form_caption' => 'Select an image to upload:',
+				'img_class' => 'ps_images',
+				'img_alerts' => 3600,
+				'show_caption' => 1,
+				'show_imgcaption' => 1,
+				'contrib_role' => 10,
+				'img_status' => 0,
+				'last_alert' => 0,
+				'use_advanced' => 0,
+				'use_customform' => 0
+			);
+			update_option($this->adminOptionsName, $psAdminOptions);
+		}
+		
+		return $psAdminOptions;
+	}
+
+	
+}
+
+?>
---
+++ 0.3.00/ajax.php
@@ -0,0 +1,303 @@
+<?php
+
+if (!function_exists('add_action'))
+{
+	require_once("../../../wp-load.php");
+}
+
+check_ajax_referer( "bwbps_moderate_images" );
+
+if(!function_exists('json_encode')){
+	require("classes/JSON.php");
+}
+
+$bwbpsuploaddir = wp_upload_dir();
+define('PSUPLOADPATH', $bwbpsuploaddir['basedir']);
+define('PSIMAGESPATH',PSUPLOADPATH."/bwbps/");
+define('PSTHUMBSPATH',PSUPLOADPATH."/bwbps/thumbs/");
+define("PSCUSTOMDATATABLE", $wpdb->prefix."bwbps_customdata");
+define("PSIMAGESTABLE", $wpdb->prefix."bwbps_images");
+
+
+class BWBPS_AJAX{
+	
+	var $psUploader;
+	var $allowNoImg = false;
+	
+	function BWBPS_AJAX(){
+
+		if(isset($_POST['action']) && $_POST['action']){
+			$action = $_POST['action'];
+		} else {
+			die(-1);
+		}
+
+
+		switch ($action){
+			case 'approve':
+				$this->approveImage();
+				break;
+		
+			case 'delete' :
+				$this->deleteImage();
+				break;
+	
+			case 'savecaption' :
+				$this->saveCaption();
+				break;
+				
+			case 'mass_updategalleries' :
+				$this->massUpdateGalleries();
+				break;
+				
+			case 'userdelete' :
+				$this->userDeleteImage(false);
+				break;
+			
+			case 'userdeletewithpost' :
+				$this->userDeleteImage(true);
+				break;
+				
+			case 'setgalleryid' :
+				$this->setGalleryID();
+				break;
+		
+			default :
+				break;
+		}
+	}
+	
+	function setGalleryID(){
+		global $wpdb;
+		
+		if(current_user_can('level_10') && isset($_POST['gallery_id'])){
+		
+			$data['gallery_id'] = (int)$_POST['gallery_id'];
+			if(!$data['gallery_id']){
+				$json['message'] = "Invalid Gallery ID.";
+				$json['action'] = 'failed';
+			} else {
+				$json['image_id'] = (int)$_POST['image_id'];
+				$where['image_id'] = $json['image_id'];
+				$json['status'] = $wpdb->update(PSIMAGESTABLE, $data, $where);
+				
+				$json['message'] = "";				
+				$json['action'] = 'galleryset';
+			}
+			
+			$json['deleted'] = '';
+		
+			echo json_encode($json);
+			return;
+		}else {$json['status'] = -1;}
+		echo json_encode($json);
+
+	}
+	
+	function saveCaption(){
+		global $wpdb;
+		if(current_user_can('level_1')){
+		
+			$data['image_caption'] = stripslashes($_POST['image_caption']);
+			$data['url'] = stripslashes($_POST['image_url']);
+			$json['image_id'] = (int)$_POST['image_id'];
+			$where['image_id'] = $json['image_id'];
+			$json['status'] = $wpdb->update(PSIMAGESTABLE, $data, $where);
+			$json['action'] = 'saved';
+			$json['deleted'] = '';
+		
+			echo json_encode($json);
+			return;
+		}else {$json['status'] = -1;}
+		echo json_encode($json);
+
+	}
+
+	function approveImage(){
+		global $wpdb;
+		if(current_user_can('level_10')){
+		
+			$data['status'] = 1;
+			$json['image_id'] = (int)$_POST['image_id'];
+			$where['image_id'] = $json['image_id'];
+			$json['status'] = $wpdb->update(PSIMAGESTABLE, $data, $where);
+			$json['action'] = 'approved';
+			$json['deleted'] = '';
+		
+			echo json_encode($json);
+			return;
+		}else {$json['status'] = -1;}
+		echo json_encode($json);
+	}
+
+	function deleteImage(){
+		global $wpdb;
+		if(current_user_can('level_10')){
+			$imgid = (int)$_POST['image_id'];
+			$json['image_id'] = $imgid;
+			if($imgid){
+				$filename = $wpdb->get_var($wpdb->prepare("SELECT file_name FROM "
+					.PSIMAGESTABLE. " WHERE image_id = %d", $imgid));
+				if($filename){
+					unlink(PSIMAGESPATH.$filename);
+					unlink(PSTHUMBSPATH.$filename);
+				}
+			
+				$json['status'] = $wpdb->query($wpdb->prepare('DELETE FROM '.
+					PSIMAGESTABLE.' WHERE image_id = %d', $imgid ));
+				
+				$wpdb->query($wpdb->prepare('DELETE FROM '. PSCUSTOMDATATABLE
+					.' WHERE image_id = %d', $imgid));
+					
+				if( !$filename ){ $filename = ""; } else { $filename = " - ".$filename; }
+				$json['action'] = 'deleted'.$filename;
+				$json['deleted'] = 'deleted';
+				
+			} else {$json['status'] = 0;}
+		} else {
+			$json['status'] = 0;
+		}
+		
+		echo json_encode($json);
+		return;
+	}
+	
+	/*
+	 *	User Delete Image
+	 *	- Allows user to delete his/her own image if it's not approved
+	 *	- Provides some checking that the admin doesn't need
+	 *	- Also allows you to delete a post that might have been created for that  
+	 *	  image in a Custom Upload script
+	*/
+	function userDeleteImage($deletePost){
+		global $wpdb, $user_ID;
+				
+		if(current_user_can('level_0')){
+			$imgid = (int)$_POST['image_id'];
+			$json['image_id'] = $imgid;
+			if($imgid){
+				$row = $wpdb->get_row($wpdb->prepare("SELECT file_name, post_id FROM "
+					.PSIMAGESTABLE. " WHERE image_id = %d AND user_id = %d AND status < 0 ", $imgid, $user_ID));
+					
+				if(!$row){
+					//Bomb out if no row returned
+					$json['status'] = 0;
+					return;
+				}
+				if($row->file_name){
+					unlink(PSIMAGESPATH.$filename);
+					unlink(PSTHUMBSPATH.$filename);
+				}
+			
+				$json['status'] = $wpdb->query($wpdb->prepare('DELETE FROM '.
+					PSIMAGESTABLE.' WHERE image_id = %d AND user_ID = %d AND status < 0 ', $imgid, $user_ID ));
+				if($json['status']){
+					$wpdb->query($wpdb->prepare('DELETE FROM '. PSCUSTOMDATATABLE
+						.' WHERE image_id = %d', $imgid));
+						
+					//Delete the related post if directed to
+					if( $deletePost && $row->post_id ){
+						
+						//Check to make sure this person is deleting only his/her own post
+						//Also check to make sure that this post is "Pending"
+						$postAuthor = $wpdb->get_var($wpdb->prepare("SELECT post_author FROM "
+					. $wpdb->posts . " WHERE ID = %d AND post_author = %d AND post_status = 'pending' ", $row->post_id, $user_ID));
+					
+						if($postAuthor){
+							wp_delete_post((int)$row->post_id);
+						}
+					
+					}
+				}
+					
+				if( !$filename ){ $filename = ""; } else { $filename = " - ".$filename; }
+				$json['action'] = 'deleted'.$filename;
+				$json['deleted'] = 'deleted';
+				
+			} else {$json['status'] = 0;}
+		} else {
+			$json['status'] = 0;
+		}
+		
+		echo json_encode($json);
+		return;
+	}
+	
+	function massUpdateGalleries(){
+		global $wpdb;
+		
+		$json['action'] = 'massupdategalleries';
+		$json['count'] = 0;
+		$json['message'] = 'Not authorized.';
+		
+		if(!current_user_can('level_10')){
+			echo json_encode($json);
+			return;
+		}
+		
+		$gflds = $this->getGallerySettingFields();
+		$galflds = $gflds['n'];
+		$galtypes = $gflds['t'];
+			
+		$json['message'] = 'Invalid field name.';
+		if(!isset($_POST['field_name']) || strlen($_POST['field_name']) < 3){
+			echo json_encode($json);
+			return;
+		}
+		
+		$field = substr($_POST['field_name'], 3);
+		
+		if(!is_array($galflds) || !in_array($field, $galflds)){
+			echo json_encode($json);
+			return;
+		}
+		
+		$type = $galtypes[array_search($_POST['field_name'], $galflds)];
+		
+		
+		if($type == int){
+			if($_POST['field_value'] == 'true'){ 
+				$data[$field] = 1;		
+			} else {
+				if($_POST['field_value'] == 'false'){
+					$data[$field] = 0;	
+				} else {
+					$data[$field] = (int)$_POST['field_value'];	
+				}
+				
+			}
+		} else {
+			$data[$field] = $_POST['field_value'];
+		}
+		
+		$where['status'] = 1;
+		
+		$json['count'] = $wpdb->update(PSGALLERIESTABLE, $data, $where);
+		
+		$json['message'] = "Galleries updated: ". $json['count'];
+		echo json_encode($json);
+		return;
+	}
+	
+	function getGallerySettingFields(){
+		global $wpdb;
+		$sql = "SELECT * FROM ".PSGALLERIESTABLE." LIMIT 1";
+		
+		$ret = $wpdb->get_row($sql);
+		
+		foreach($wpdb->get_col_info('name') as $name){
+			$colname[] = $name;
+		}
+		foreach($wpdb->get_col_info('type') as $type){
+			$coltype[] = $type;
+		}
+		$c['n'] = $colname;
+		$c['t'] = $coltype;
+		return $c;
+	}
+
+}
+
+$bwbpsAjax = new BWBPS_Ajax();
+
+?>
