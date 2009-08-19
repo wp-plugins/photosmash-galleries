9373cbbb422935818b433e210cebc596b07ed58d
bennebw <bennebw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-19 17:59:42 +0000

    0.3.04 - fixed conflict in adding the esc_attr group of functions.  Added 2 template tags show_photosmash_gallery() and get_photosmash_gallery()
    git-svn-id: https://plugins.svn.wordpress.org/photosmash-galleries/trunk@147281 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- star_rating_widget-demos Folder/demo2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Cache-Control: no-cache'); // Prevent IE from caching XMLHttpRequest response
-
-$buff[] = 'Response from server: ';
-$buff[] = $_POST['rate'] ? "Rate recived: {$_POST['rate']}" : '$_POST[\'rate\'] is empty';
-$buff[] = $_SERVER['HTTP_X_REQUESTED_WITH'] ? 'This is AJAX request' : 'This is POST request<br><a href="javascript:history.back();">&laquo; Back</a>';
-
-echo implode('<br>', $buff);
